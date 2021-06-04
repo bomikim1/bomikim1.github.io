7057b120bfbdd79a41f365782a02174536ee4907
Bomi Kim <95bomi@gmail.com>
2021-06-04 17:58:31 +0900

    edit profile image

--- src/yourdata.js
+++ src/yourdata.js
@@ -122,8 +122,11 @@ export default {
     "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   aboutParaThree:
     "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  aboutImage:
-    <img src={aboutImage} />,
+  aboutImage:[
+    {
+      img: aboutImage,
+      
+    }],
 
   //   End About Section ---------------------
 
