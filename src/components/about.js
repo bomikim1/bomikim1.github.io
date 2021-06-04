11abeac8d6d8f7022d4788eaa3efcd78d6e0774c
Bomi Kim <95bomi@gmail.com>
2021-06-04 18:42:39 +0900

    edit profile image

--- src/components/about.js
+++ src/components/about.js
@@ -22,7 +22,7 @@ const About = () => {
             </p>
           </div>
           <div className="image-wrapper">
-              <img src="./images/profile.jpeg" alt="about"/>
+          <img src={require("./images/profile.jpeg").default} alt="about"/>
           </div>
         </div>
       </div>
