8e3ca70234cd2be8be42aab8bf8aa3399c202623
Bomi Kim <95bomi@gmail.com>
2021-06-04 17:13:32 +0900

    social delete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,18 +14,6 @@ const Footer = () => {
           <a className="email-link" href={`mailto:${data.contactEmail}`}>
             {data.contactEmail}
           </a>
-          <div className="social-icons">
-            {data.social.map((socialLink, index) => (
-              <a
-                key={index}
-                href={socialLink.url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={socialLink.img} alt="icons"></img>
-              </a>
-            ))}
-          </div>
           <span>
             Made With <icon>❤</icon> by{" "}
             <a>Bomi Kim</a>
